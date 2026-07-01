bfe594978296166e245815ae419eeb58ad04e5d7
mikolasevic13 <josip.mikolasevic@gmail.com>
2026-07-01 09:04:31 +0200

    test promijenjen

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react-native";
+import App from "./App";
 
 it('renders welcome message', () => {
   render(<App />);
   expect(screen.getByText('Learn React')).toBeInTheDocument();
 });
+
+
+
